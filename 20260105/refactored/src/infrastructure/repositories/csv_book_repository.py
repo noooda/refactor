1f7a2a9b386d565b5ab798c38e451a37c3681049
noooda <noooda211@gmail.com>
2026-01-15 00:17:43 +0900

    refactor: Change type int to str

--- 20260105/refactored/src/infrastructure/repositories/csv_book_repository.py
+++ 20260105/refactored/src/infrastructure/repositories/csv_book_repository.py
@@ -5,7 +5,7 @@ class CsvBookRepository:
     def __init__(self, path: str) -> None:
         self._path = path
 
-    def save(self, title: str, author: str, price: int) -> None:
+    def save(self, title: str, author: str, price: str) -> None:
         with open(self._path, mode="a", encoding="utf-8") as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow([title, author, price])
